ac31be347cda211a0e9dfc01bafb4a910f1d652c
Ryan Klepacs <rklepacs@gmail.com>
2024-02-02 02:26:50 -0500

    contained the code inside of it's own total return function. Also refactored some of the code to make it cleaner

--- Total_Return.py
+++ Total_Return.py
@@ -8,27 +8,27 @@ import yfinance as yf
 #import plotly.graph_objs as grobj
 
 
+def total_return(ticker):
 
-user_ticker = input("Enter the ticker of the stock: ").upper()
+    user_ticker = ticker
+    user_shares = float(input("Enter the amount of shares you hold: "))
+    user_avg_cost = float(input("Enter the average cost of your shares: "))
 
-data = yf.Ticker(user_ticker)
+    data = yf.Ticker(user_ticker)
 
-user_shares = float(input("Enter the amount of shares you hold: "))
-user_avg_cost = float(input("Enter the average cost of your shares: "))
+    initial_investment = user_shares * user_avg_cost
 
-initial_investment = user_shares * user_avg_cost
+    current_price = data.info.get('currentPrice', 'price data not found')
+    shares_need_sold = 0
 
-current_price = data.info.get('currentPrice', 'price data not found')
-shares_need_sold = 0
+    print(f"The current price of {user_ticker} is {current_price}")
 
-print(f"The current price of {user_ticker} is {current_price}")
+    if float(current_price) <= user_avg_cost:
+        print("At current market prices you cannot recoup your total investment.")
+    else:
+        shares_need_sold = initial_investment/current_price
+        print(f"At current market prices if you sold {shares_need_sold} of your shares of {user_ticker} you would reclaim your initial investment!")
 
-if float(current_price) <= user_avg_cost:
-    print("At current market prices you cannot recoup your total investment.")
-else:
-    shares_need_sold = initial_investment/current_price
-    print(f"At current market prices if you sold {shares_need_sold} of your shares of {user_ticker} you would reclaim your initial investment!")
-
-
+total_return(input("Enter the ticker of the stock: ").upper())
 
 
